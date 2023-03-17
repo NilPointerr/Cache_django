58d6a8478a385a98406e621f9e0e5e033f71e679
NilPointerr <nilesh.ultragames@gmail.com>
2023-03-17 18:43:45 +0530

    add low level cache

--- caching1/urls.py
+++ caching1/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import temp_1,view_cache
+from .views import temp_1,view_cache,low_level_cache,del_cache
 from caching1 import views
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('',cache_page(30)(temp_1.as_view()),name = 'home'),
-    path('view',views.view_cache,name="view")
+    path('view',views.view_cache,name="view"),
+    path('low',views.low_level_cache,name="low"),
+    path('del',views.del_cache,name='del'),
 
 
 ]
--- caching1/views.py
+++ caching1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.cache import cache
 # per view cache
 from django.views.decorators.cache import cache_page
 
@@ -11,4 +12,28 @@ class temp_1(TemplateView):
 
 # @cache_page(30)
 def view_cache(request):
+    return render(request,'home.html')
+
+
+'''low level cache'''
+
+# def low_level_cache(request):
+#     data = cache.get('city','uddhav',30)
+#     if data is None:
+#         cache.set('city','Ahmedabad',30)
+#         data = cache.get('city')
+#     return render(request,'home.html',{'data':data})
+
+# def low_level_cache(request):
+#     data = cache.get_or_set("city","palanpur",30)
+#     return render(request,'home.html',{'data':data})
+
+def low_level_cache(request):
+    data = {'name':'Nilesh','Age':21}
+    cache.set_many(data,30)
+    info = cache.get_many(data)
+    return render(request,'home.html',{'info':info})
+
+def del_cache(request):
+    cache.clear()
     return render(request,'home.html')
